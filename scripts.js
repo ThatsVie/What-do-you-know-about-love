c0b2cb46cd21931708161b09694aeef1bc4c4c51
ThatsVie <viepaulalmt@gmail.com>
2024-07-26 16:08:10 -0500

    Implementar la función de búsqueda y leer más funciones

--- scripts.js
+++ scripts.js
@@ -1,129 +1,199 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // Functionality for question links
-  const questionLinks = document.querySelectorAll('.question-link');
-  const searchResultsSection = document.querySelector('#search-results-container');
-  const searchContainer = document.querySelector('#search-results');
-
-  questionLinks.forEach(link => {
-    link.addEventListener('click', function(event) {
-      event.preventDefault();
-      const targetClass = this.getAttribute('data-target');
-
-      if (searchContainer) {
-        // Clear the search results container
-        searchContainer.innerHTML = '';
-
-        // Collect related answers
-        const relatedAnswers = document.querySelectorAll(`.${targetClass}`);
-        relatedAnswers.forEach(answer => {
-          searchContainer.appendChild(answer.cloneNode(true));
+    console.log('DOM fully loaded and parsed');
+  
+    const truncateLength = 200;
+    const responseCards = document.querySelectorAll('.response-card');
+    console.log('Response Cards:', responseCards);
+  
+    function setupReadMoreLinks(cards) {
+      cards.forEach(card => {
+        const cardText = card.querySelector('.card-text[data-full-text]');
+        if (cardText) {
+          const fullText = cardText.getAttribute('data-full-text');
+          if (fullText) {
+            const truncatedText = fullText.substring(0, truncateLength) + '...';
+  
+            if (fullText.length > truncateLength) {
+              cardText.textContent = truncatedText;
+  
+              const readMoreLink = card.querySelector('.read-more');
+              readMoreLink.style.display = 'inline';
+              readMoreLink.addEventListener('click', function(event) {
+                event.preventDefault();
+                console.log('Read More clicked');
+                if (cardText.textContent === truncatedText) {
+                  cardText.textContent = fullText;
+                  readMoreLink.textContent = 'Read Less';
+                } else {
+                  cardText.textContent = truncatedText;
+                  readMoreLink.textContent = 'Read More';
+                }
+              });
+            } else {
+              cardText.textContent = fullText;
+              card.querySelector('.read-more').style.display = 'none';
+            }
+          }
+        }
+      });
+    }
+  
+    setupReadMoreLinks(responseCards);
+  
+    const questionLinks = document.querySelectorAll('.question-link');
+    const searchResultsSection = document.querySelector('#search-results-container');
+    const searchContainer = document.querySelector('#search-results');
+  
+    console.log('Question Links:', questionLinks);
+    console.log('Search Results Section:', searchResultsSection);
+    console.log('Search Container:', searchContainer);
+  
+    if (questionLinks) {
+      questionLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+          event.preventDefault();
+          const targetClass = this.getAttribute('data-target');
+          console.log('Question Link Clicked:', targetClass);
+  
+          if (searchContainer) {
+            searchContainer.innerHTML = '';
+            const relatedAnswers = document.querySelectorAll(`.${targetClass}`);
+            console.log('Related Answers:', relatedAnswers);
+            relatedAnswers.forEach(answer => {
+              searchContainer.appendChild(answer.cloneNode(true));
+            });
+  
+            searchContainer.style.display = 'flex';
+            searchContainer.style.flexWrap = 'wrap';
+            searchContainer.style.justifyContent = 'center';
+  
+            if (relatedAnswers.length > 0) {
+              searchResultsSection.style.display = 'block';
+            } else {
+              searchResultsSection.style.display = 'none';
+            }
+  
+            searchResultsSection.scrollIntoView({ behavior: 'smooth' });
+  
+            // Reattach the event listeners to the dynamically added cards
+            const newCards = searchContainer.querySelectorAll('.response-card');
+            setupReadMoreLinks(newCards);
+          }
         });
-
-        // Apply the flex layout to the search results container
+      });
+    }
+  
+    const navLinks = document.querySelectorAll('a.nav-link');
+  
+    if (navLinks) {
+      navLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+          const target = this.getAttribute('href');
+          if (target.startsWith('#')) {
+            event.preventDefault();
+            const targetElement = document.querySelector(target);
+            if (targetElement) {
+              targetElement.scrollIntoView({ behavior: 'smooth' });
+            }
+          } else {
+            window.location.href = target;
+          }
+        });
+      });
+    }
+  
+    const urlParams = new URLSearchParams(window.location.search);
+    const target = urlParams.get('target');
+    if (target) {
+      const targetContainer = document.getElementById(target);
+      if (targetContainer) {
+        targetContainer.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  
+    const searchForm = document.querySelector('#search-form');
+    const searchInput = document.querySelector('#search-input');
+    const searchQuestion = document.querySelector('#search-question');
+    const clearResultsButton = document.querySelector('#clear-results-button');
+  
+    console.log('Search Form:', searchForm);
+    console.log('Search Input:', searchInput);
+    console.log('Search Question:', searchQuestion);
+    console.log('Clear Results Button:', clearResultsButton);
+  
+    if (searchForm) {
+      searchForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const searchTerm = searchInput.value.trim().toLowerCase();
+        const searchQuestionValue = searchQuestion.value.trim().toLowerCase();
+        console.log('Search Term:', searchTerm);
+        console.log('Search Question Value:', searchQuestionValue);
+  
+        searchContainer.innerHTML = '';
+  
+        const allAnswers = document.querySelectorAll('.response-card');
+        console.log('All Answers:', allAnswers);
+        let hasResults = false;
+  
+        allAnswers.forEach(answer => {
+          const cardTitleElement = answer.querySelector('.card-title');
+          const cardTextElement = answer.querySelector('.card-text');
+          console.log('Card Title Element:', cardTitleElement);
+          console.log('Card Text Element:', cardTextElement);
+  
+          if (cardTitleElement && cardTextElement) {
+            const cardTitle = cardTitleElement.textContent.toLowerCase();
+            const cardText = cardTextElement.getAttribute('data-full-text') || cardTextElement.textContent;
+            console.log('Card Title:', cardTitle);
+            console.log('Card Text:', cardText);
+  
+            if (cardText) {
+              const cardTextLower = cardText.toLowerCase();
+  
+              if (
+                (searchTerm && cardTextLower.includes(searchTerm)) ||
+                (searchQuestionValue && cardTitle.includes(searchQuestionValue))
+              ) {
+                console.log('Answer Found:', cardTitle);
+                const answerClone = answer.cloneNode(true);
+                searchContainer.appendChild(answerClone);
+                hasResults = true;
+              }
+            }
+          }
+        });
+  
         searchContainer.style.display = 'flex';
         searchContainer.style.flexWrap = 'wrap';
         searchContainer.style.justifyContent = 'center';
-
-        // Show the search results container if there are results
-        if (relatedAnswers.length > 0) {
+  
+        if (hasResults) {
           searchResultsSection.style.display = 'block';
+          console.log('Search Results Section Displayed');
         } else {
           searchResultsSection.style.display = 'none';
+          console.log('No Results Found');
         }
-
-        // Scroll to the search results container
+  
         searchResultsSection.scrollIntoView({ behavior: 'smooth' });
-      }
-    });
-  });
-
-  // Functionality for navbar links with smooth scrolling
-  const navLinks = document.querySelectorAll('a.nav-link');
-
-  navLinks.forEach(link => {
-    link.addEventListener('click', function(event) {
-      const target = this.getAttribute('href');
-      if (target.startsWith('#')) {
-        event.preventDefault();
-        const targetElement = document.querySelector(target);
-        if (targetElement) {
-          targetElement.scrollIntoView({ behavior: 'smooth' });
-        }
-      } else {
-        window.location.href = target;
-      }
-    });
-  });
-
-  // Handle URL parameter for target container
-  const urlParams = new URLSearchParams(window.location.search);
-  const target = urlParams.get('target');
-  if (target) {
-    const targetContainer = document.getElementById(target);
-    if (targetContainer) {
-      targetContainer.scrollIntoView({ behavior: 'smooth' });
-    }
-  }
-
-  // Functionality for search form
-  const searchForm = document.querySelector('#search-form');
-  const searchInput = document.querySelector('#search-input');
-  const searchQuestion = document.querySelector('#search-question');
-  const clearResultsButton = document.querySelector('#clear-results-button');
-
-  if (searchForm) {
-    searchForm.addEventListener('submit', function(event) {
-      event.preventDefault();
-      const searchTerm = searchInput.value.toLowerCase();
-      const searchQuestionValue = searchQuestion.value.toLowerCase();
-
-      // Clear the search results container
-      searchContainer.innerHTML = '';
-
-      // Filter and display answers based on search term and selected question
-      const allAnswers = document.querySelectorAll('.response-card');
-      let hasResults = false;
-
-      allAnswers.forEach(answer => {
-        const cardText = answer.textContent.toLowerCase();
-
-        if (
-          (searchTerm && cardText.includes(searchTerm)) ||
-          (searchQuestionValue && cardText.includes(searchQuestionValue))
-        ) {
-          const answerClone = answer.cloneNode(true); // Clone the card with its container
-          searchContainer.appendChild(answerClone);
-          hasResults = true;
-        }
+  
+        // Reattach the event listeners to the dynamically added cards
+        const newCards = searchContainer.querySelectorAll('.response-card');
+        setupReadMoreLinks(newCards);
       });
-
-      // Apply the flex layout to the search results container
-      searchContainer.style.display = 'flex';
-      searchContainer.style.flexWrap = 'wrap';
-      searchContainer.style.justifyContent = 'center';
-
-      // Show the search results container if there are results
-      if (hasResults) {
-        searchResultsSection.style.display = 'block';
-      } else {
+  
+      clearResultsButton.addEventListener('click', function() {
+        searchContainer.innerHTML = '';
         searchResultsSection.style.display = 'none';
-      }
-
-      // Scroll to the search results container
-      searchResultsSection.scrollIntoView({ behavior: 'smooth' });
-    });
-
-    // Functionality for clear results button
-    clearResultsButton.addEventListener('click', function() {
-      // Clear the search results container
-      searchContainer.innerHTML = '';
-      // Hide the search results container
-      searchResultsSection.style.display = 'none';
-      // Clear the search input and question select
-      searchInput.value = '';
-      searchQuestion.value = '';
-    });
-  }
-});
-
-
+        searchInput.value = '';
+        searchQuestion.value = '';
+      });
+    }
+  });
+  
+  
+  
+  
+  
+  
+  
